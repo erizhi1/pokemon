1f329a439d05fb62c663a891fb378622266caab8
Erich <erich.gomez.aguilera@gmail.com>
2025-07-10 12:58:30 -0400

    test

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,55 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <header class="header">
+    <label for="">
+      Escribe el Pokemon que quieras buscar Por ID:
+      <input type="text" name="" id="">
+      <button>Buscar Pokemon</button>
+    </label>
+  </header>
+  <main class="main">
+   <section class="pokemonCard">
+    <div class="nameImage">
+      <h2 class="pokemonName">pikachu</h2>
+      <img src="" alt="pikachu" >
+    </div>
+    <ul class="type">
+      <h2>Tipo:</h2>
+      <li>
+        <span>Electrico</span>
+      </li>
+    </ul>
+    <ul class="stats">
+      <h2>Stats</h2>
+      <li>
+        <span></span>
+      </li>
+    </ul>
+   </section>
+  </main>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+import { pokeapi } from "@/api/pokeaapi";
+import { search } from "core-js/fn/symbol";
+export default{
+  name: "App",
+  data() {
+    return {
+      pokemonData: {},
+      pokemonID: '',
+    }
+  },
+  methods: {
+    async searchPokemon() {
+      try {
+        const response = await pokeapi.get(`/${this.pokemonID}`);
+        this.pokemonData = response.data;
+      } catch (error) {
+        console.error("Error fetching Pokemon data:", error);
+      }
+    }
   }
 }
-</script>
 
+</script>
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
